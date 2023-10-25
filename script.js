9866815a46ab59f875001f5774607d87fa8ae1c6
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-10-26 01:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListner("DOMContentLoaded",function(){
+	let target=document.getElementById("level");
+	if(target){
+		let domLevel=0;
+		let currentElement=tareget;
+		while(currentElement!==document.documentElement){
+			currentElement=currentElement.parentElement;
+			domLevel++;
+		}
+		alert("The level of the element is: " + domLevel);
+	}
+})
